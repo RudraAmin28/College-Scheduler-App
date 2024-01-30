424bc7c927ce2ee3e20914dc61bd2ebfb98d8ed6
RudraAmin28 <rudrayogini@gmail.com>
2024-01-30 17:34:37 -0500

    frontend for edit TODO functionality

--- app/src/main/java/com/example/collegeschedulerapp/Task.java
+++ app/src/main/java/com/example/collegeschedulerapp/Task.java
@@ -9,6 +9,10 @@ public class Task {
         this.isChecked = isChecked;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getTitle() {
         return title;
     }
